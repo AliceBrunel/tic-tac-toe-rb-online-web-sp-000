07c48567b42b726a81d6dc35ad9e8e1b25beeccb
Alice Brunel <alc.brunel@gmail.com>
2019-07-05 01:12:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,15 +40,15 @@ def turn(board)
 end
 
 def turn_count(board)
-  # counts occupied positions
-  count = 0
-  board.each do |index|
-    if index != " "
-      count += 1
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
     end
   end
-    count
- end
+  turns
+
+end
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
